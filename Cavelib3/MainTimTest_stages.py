f8f6ca53d4385c842fc86bfabf85063f3716a7a8
Jesper Huijgens Laptop <jesper_huijgens@hotmail.com>
2013-12-19 15:33:19 +0100

    Holes fixed

--- Cavelib3/MainTimTest_stages.py
+++ Cavelib3/MainTimTest_stages.py
@@ -129,7 +129,7 @@ class CustomCaveApplication(caveapp.CaveApplication):
 		# Called when we enter a proximity
 		def EnterProximity(e):
 			for i in range(nrAxes):
-				if e.sensor == self.axesensors[i]:
+				if e.sensor == sensors[i]:
 					self.axesHit[i] += 1
 					print "Hit axe #" + str(i) + " " + str(self.axesHit[i]) + " times!"
 					
